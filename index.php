7c4a3ede84773bae66a2dc58a85b0a9af2091966
Julien Rousseau <julien.rousseau@augergroupe.com>
2020-08-07 00:35:57 -0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php require "req/header.php"; ?>
+
+<?php require "req/summary.php"; ?>
+
+<?php require "req/footer.php"; ?>
---
+++ req/summary/summary-list.php
@@ -0,0 +1,7 @@
+<ul>
+    <li>
+        <img src="assets/img" alt="Inserer initiales ici avec php">
+        <span class="amount">150$</span>
+        <span class="amount" inDepth="0">+150$</span>
+    </li>
+</ul>
